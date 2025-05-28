204dcc4ac6b31a9ee1c77600d70f8573f6f18783
DanielaVillalta <20240286@ricaldone.edu.sv>
2025-05-27 21:36:15 -0600

    Ejercicio 5 corregido

--- Ejercicio5_TreeMap/src/main/java/Ejercicio5/Main.java
+++ Ejercicio5_TreeMap/src/main/java/Ejercicio5/Main.java
@@ -1,5 +1,6 @@
 package Ejercicio5;
 
+import java.util.Map;
 import java.util.TreeMap;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -12,15 +13,38 @@ public class Main {
 
     public static void mostrarTreeMap() {
         TreeMap<String, Number> ciudades = new TreeMap<>();
+        Double mayorTemperatura = 0.0;
+        String ciudadMasCalida = null;
+        Double menorTemperatura = Double.MAX_VALUE;
+        String ciudadMasFria = null;
 
         ciudades.put("San Salvador", 40);
-        ciudades.put("Nueva York", 15);
-        ciudades.put("Buenos Aires", 10);
+        ciudades.put("Nueva York", 10);
+        ciudades.put("Buenos Aires", 15);
         ciudades.put("Mejicanos", 36);
         ciudades.put("Ciudad de Guatemala", 20);
 
-        System.out.println("Ciudades en orden alfabético" + ciudades);
-        System.out.println("Ciudad más fría: " + ciudades.firstEntry());
-        System.out.println("Ciudad más cálida: " + ciudades.lastEntry());
+        System.out.println("\nCiudades en orden alfabético:");
+        for(Map.Entry<String, Number> entrada : ciudades.entrySet()) {
+            System.out.println(entrada.getKey() + " - " + entrada.getValue() + "°C");
+        }
+
+        for (Map.Entry<String, Number> entrada : ciudades.entrySet()) {
+            double temperatura = entrada.getValue().doubleValue();
+            String ciudad = entrada.getKey();
+
+            if (temperatura > mayorTemperatura) {
+                mayorTemperatura = temperatura;
+                ciudadMasCalida = ciudad;
+            }
+
+            if (temperatura < menorTemperatura) {
+                menorTemperatura = temperatura;
+                ciudadMasFria = ciudad;
+            }
+        }
+
+        System.out.println("\nCiudad más cálida: " + ciudadMasCalida + " - " + mayorTemperatura + "°C");
+        System.out.println("Ciudad más fría: " + ciudadMasFria + " - " + menorTemperatura + "°C");
     }
 }
