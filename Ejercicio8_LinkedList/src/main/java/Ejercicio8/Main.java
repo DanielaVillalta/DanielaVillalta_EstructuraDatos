cd7df4796c076fc438ee6ba1069af188ad2ce44d
DanielaVillalta <20240286@ricaldone.edu.sv>
2025-05-27 22:49:38 -0600

    Ejercicio 8

--- Ejercicio8_LinkedList/src/main/java/Ejercicio8/Main.java
+++ Ejercicio8_LinkedList/src/main/java/Ejercicio8/Main.java
@@ -1,13 +1,38 @@
 package Ejercicio8;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Historial de navegación");
+        mostrarList();
     }
 
     public static void mostrarList() {
+        LinkedList<String> historial = new LinkedList<>();
+        Scanner scan = new Scanner(System.in);
 
+        historial.add("google");
+        historial.add("youtube");
+        historial.add("github");
+        historial.add("teams");
+        historial.add("gmail");
+
+        System.out.println("\nLista actual: ");
+        for(String elemento : historial) {
+            System.out.println(elemento);
+        }
+
+        System.out.println("\nAtrás (Presiona ENTER)");
+        scan.nextLine();
+        historial.removeLast();
+
+        System.out.println("\nLista después de retroceder:");
+        for(String elemento : historial) {
+            System.out.println(elemento);
+        }
     }
 }
